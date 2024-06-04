7e705753cde6868b8e94b2d7aa40a979cba5d294
DuvanMotavita <duvanfel1@yahoo.com.co>
2024-06-04 18:56:42 -0500

    First commit

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+} from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css',
+})
+export class ListComponent {
+  @Output()
+  public onDelete: EventEmitter<string> = new EventEmitter();
+
+  @Input()
+  public characterList: Character[] = [
+    {
+      id: 'z',
+      name: 'Trunks',
+      power: 10,
+    },
+  ];
+
+  onDeleteCharacter(id?: string): void {
+    if (!id) return;
+    this.onDelete.emit(id);
+  }
+}
